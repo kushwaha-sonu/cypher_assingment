8e33bb2cddcbad0f411b8af29d2b64d68cde2ce9
Sonu kumar <104054249+kushwaha-sonu@users.noreply.github.com>
2024-08-20 21:55:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://cypher-fullstack-assingment.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: "*", // Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
   })
